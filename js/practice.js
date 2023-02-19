3e49a70039df9537c029257213bd1c012746b4bf
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-20 05:35:21 +0600

    4 problems solve already

---
+++ js/practice.js
@@ -0,0 +1,39 @@
+/* 
+problem:1
+const sum=(num1,num2,num3)=>num1+num2+num3;
+const result=sum(20,30,50);
+console.log(result);
+ */
+
+
+/* problem:2
+const multipleLine=`I am a web developer
+I love to code
+I love to eat kacci biriyani
+`
+console.log(multipleLine); */
+
+
+/* problem:3
+const add=(num1=0,num2=10)=>num1+num2;
+const result=add(10);
+console.log(result); */
+
+// problem:4
+// const friendNameLength=(friends)=>{
+//      let newFriend=[];
+//     for(let friend of friends){
+//         if(friend.length%2==0){
+//             newFriend.push(friend);
+//         }
+//     }
+//     return newFriend;
+// }
+// const friends=['abul','babul','peba','boba','mirza'];
+// const result=friendNameLength(friends);
+// console.log(result);
+
+
+
+
+
