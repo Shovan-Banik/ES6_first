9210595878a1321d693206b7df589ae47329f72a
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-20 03:52:44 +0600

    more arrow function

---
+++ js/moreArow.js
@@ -0,0 +1,15 @@
+// no parameter arrow function
+const pie=()=>3.1416;
+// one parameter arrow function
+const double=(number)=>number*2;
+// simplified version of one parameter arrow function(bracket deya lge na r ki)
+const triple=number=>number*3;
+// simple arrow function
+const sum=(a,b,c,d,e)=>a+b+c+d+e;
+// multiline arrow function(multiline ar khetre return krte hy...onno gulay return kra lge na....)
+const doMath=(a,b,c)=>{
+    const sum=a+b+c;
+    const mul=a*b*c;
+    const div=mul/sum;
+    return div;
+}
