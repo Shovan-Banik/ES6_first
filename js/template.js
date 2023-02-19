3b3a725d000223e3df77be4a620b6f4ecaff0a32
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-20 02:40:46 +0600

    try to know about back tick

---
+++ js/template.js
@@ -0,0 +1,10 @@
+const a=20;
+const b=30;
+console.log(`sum of ${a} and ${b} is: ${a+b}`);
+// `` = back tick use kore sohojey maltiline code lekha jay...r dynamic string create kora jay
+
+const c=20;
+const d=30;
+const sum=c+d;
+console.log("sum of "+ a +" and "+b+" is: "+ sum);
+// 
