d84cf875b81c63c60a2a4b17960aa93bb77131c6
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-20 07:28:31 +0600

    all problem solve and done for today

--- js/practice.js
+++ js/practice.js
@@ -6,7 +6,9 @@ console.log(result);
  */
 
 
-/* problem:2
+
+/* 
+problem:2
 const multipleLine=`I am a web developer
 I love to code
 I love to eat kacci biriyani
@@ -17,49 +19,85 @@ console.log(multipleLine); */
 /* problem:3
 const add=(num1=0,num2=10)=>num1+num2;
 const result=add(10);
-console.log(result); */
+console.log(result); 
+*/
 
-// problem:4
-// const friendNameLength=(friends)=>{
-//      let newFriend=[];
-//     for(let friend of friends){
-//         if(friend.length%2==0){
-//             newFriend.push(friend);
-//         }
-//     }
-//     return newFriend;
+
+
+
+/* 
+problem:4
+const friendNameLength=(friends)=>{
+     let newFriend=[];
+    for(let friend of friends){
+        if(friend.length%2==0){
+            newFriend.push(friend);
+        }
+    }
+    return newFriend;
+}
+const friends=['abul','babul','peba','boba','mirza'];
+const result=friendNameLength(friends);
+console.log(result);
+ */
+
+
+/* 
+problem:5
+const doMath=(numbersArray)=>{
+    let newArray=[];
+    let sum=0;
+    let avg=0;
+    for(let number of numbersArray){
+        const value=number*number;
+        newArray.push(value);
+        sum=sum+value;
+    }
+    avg=sum/newArray.length;
+    return avg;
+}
+const numbers=[2,3,5];
+const result=doMath(numbers);
+console.log(result.toFixed(2)); 
+
+*/
+
+
+// problem:6
+// const maximum=(array1,array2)=>{
+//     let newArray=[...array1,...array2];
+//     const big=Math.max(...newArray);
+//     return big;
 // }
-// const friends=['abul','babul','peba','boba','mirza'];
-// const result=friendNameLength(friends);
+// const firstArray=[10,20,30];
+// const secondArray=[40,50,60];
+// const result=maximum(firstArray,secondArray);
 // console.log(result);
 
-// problem:5
-// const doMath=(numbersArray)=>{
-//     let newArray=[];
-//     let sum=0;
-//     let avg=0;
-//     for(let number of numbersArray){
-//         const value=number*number;
-//         newArray.push(value);
-//         sum=sum+value;
-//     }
-//     avg=sum/newArray.length;
-//     return avg;
-// }
-// const numbers=[2,3,5];
-// const result=doMath(numbers);
-// console.log(result.toFixed(2));
 
 
-const maximum=(array1,array2)=>{
-    let newArray=[...array1,...array2];
-    const big=Math.max(...newArray);
-    return big;
-}
-const firstArray=[10,20,30];
-const secondArray=[40,50,60];
-const result=maximum(firstArray,secondArray);
-console.log(result);
+
+// function getSum(a, b=9){
+//     return a+b;
+// } 
+// console.log(getSum(2, 7));
+
+
+// const y = x => x*x; 
+// const z = y(5);
+// console.log(z)
+
+// const name = 'Hero'; 
+// const greetings = 'Welcome Home!'+' '+name;
+// console.log(greetings)
+
+//  const num2=() => console.log(4/2)
+// num2();
+
+const cars=['toyota', 'honda','mercedes']; 
+const newCars= [...cars , 'ferrari']; 
+console.log(newCars)
+
 
 
 
